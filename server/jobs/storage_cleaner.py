2b05b165b6dd0988f7b2e35af62f369922e3a035
dmytro-ustynov <ustynov.dmitriy@gmail.com>
2022-06-25 03:10:30 +0300

    storage-cleaner: add logging, add logging config file

--- server/jobs/storage_cleaner.py
+++ server/jobs/storage_cleaner.py
@@ -1,6 +1,17 @@
 import os
+import yaml
+import logging
+from logging.config import dictConfig
 from pymongo import MongoClient
 from decouple import config
+
+LOG_YAML = 'logger_config.yaml'
+with open(LOG_YAML, 'r') as cfg:
+    logger_config = yaml.safe_load(cfg).get('logging')
+dictConfig(logger_config)
+logger = logging.getLogger('storage_cleaner')
+
+logger.info('Start cleaning UPLOAD FOLDER...')
 
 host = config('DB_HOST', 'localhost')
 port = config('DB_PORT', 27017)
@@ -22,8 +33,10 @@ for root, dirs, files in os.walk(UPLOAD_FOLDER, topdown=False):
         doc = FILES.find_one({'file_id': file_id})
         if not doc:
             # these files can be safely deleted from disk, cause they are not found in db
-            print('not found in db: ', name)
+            logger.info(f' - file not found in DB, deleting {name}.')
             os.remove(os.path.join(UPLOAD_FOLDER, name))
+
+logger.info('Cleaner finished')
 
 # Find all files that are older than 5 days
 # if such file has no registered user, it should be deleted
