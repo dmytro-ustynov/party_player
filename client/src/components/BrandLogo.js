ca13194fd46519fc08ba03efe02587e22805d6fd
dmytro-ustynov <ustynov.dmitriy@gmail.com>
2023-03-28 09:28:51 +0300

    Logo as link behavior

--- client/src/components/BrandLogo.js
+++ client/src/components/BrandLogo.js
@@ -2,9 +2,8 @@ import logoImage from "../images/logo_640.png";
 
 export default function BrandLogo(){
     return(
-        <img src={logoImage} alt={"logo"} className="image-logo"
-                     onClick={() => {
-                         window.location.replace("/")
-                     }}/>
+        <a href="/">
+            <img src={logoImage} alt={"logo"} className="image-logo"/>
+        </a>
     )
 }
