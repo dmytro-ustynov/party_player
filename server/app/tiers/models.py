f8d530417b6822f7111310aeeb41e653df3d4ab9
dmytro-ustynov <ustynov.dmitriy@gmail.com>
2023-12-10 20:38:57 +0200

    Add creating default Tiers

--- server/app/tiers/models.py
+++ server/app/tiers/models.py
@@ -17,15 +17,18 @@ from server.app.dal.database import Base
 class Tier(Base):
     __tablename__ = 'tier_descriptions'
 
-    name = Column(Enum('anonymous', 'registered', 'premium', name='tiers'), primary_key=True, default='anonymous')
+    name = Column(Enum('anonymous', 'registered', 'premium', name='tiers'),
+                  primary_key=True, default='anonymous')
     max_files = Column(Integer)
     formats = Column(ARRAY(String))
     mic_length = Column(Integer)
     adv_ratio = Column(Integer)
+    file_size = Column(Integer)
 
     def to_dict(self):
         return dict(name=self.name,
                     max_files=self.max_files,
+                    file_size=self.file_size,
                     formats=self.formats,
                     mic_length=self.mic_length,
                     adv_ratio=self.adv_ratio)
