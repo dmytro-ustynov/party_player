b4d1e6388022b9825337c05b6919c4804561a50e
dmytro-ustynov <ustynov.dmitriy@gmail.com>
2023-12-10 22:20:18 +0200

    add uvicorn logging

--- server/app/dependencies.py
+++ server/app/dependencies.py
@@ -12,7 +12,7 @@ UPLOAD_FOLDER = os.path.join(os.getcwd(), config('UPLOAD_FOLDER'))
 LOG_YAML = os.path.join(os.getcwd(), 'server', 'logger_config.yaml')
 
 with open(LOG_YAML, 'r') as cfg:
-    logger_config = yaml.safe_load(cfg).get('logging')
+    logger_config = yaml.safe_load(cfg)
 dictConfig(logger_config)
 
 logger = logging.getLogger('server')
